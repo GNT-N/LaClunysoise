c48fadd73b136095c669a921914004645a1599dc
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-04 16:36:33 +0200

    index post on page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PostRepository;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function acceuil(PostRepository $postRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'post' => $postRepository->findBy(array('page' => 'accueil'))
+        ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(PostRepository $postRepository): Response
+    {
+        return $this->render('/about.html.twig', [
+            'post' => $postRepository->findBy(array('page' => 'a-propos'))
         ]);
     }
 }
