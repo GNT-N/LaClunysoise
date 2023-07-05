5fbff0b83061b82a242f70ab5cfd8ce169517884
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-05 12:16:11 +0200

    Authentification Start

--- Readme.md
+++ Readme.md
@@ -34,3 +34,25 @@ Controller
 ```sh
 symfony console make:controller (Create controllers)
 ```
+
+Authentification
+```sh
+symfony console make:user Admin
+```
+
+hash password
+```sh
+symfony console security:hash-password
+```
+
+Rêquete SQL pour crée l'utilisateur admin en BDD
+```sh
+INSERT INTO admin (id, username, roles, password) 
+VALUES (null, 'admin', '["ROLE_ADMIN"]', 
+'-- PasswordHash with \ to escape $ --');
+```
+
+Creation de la page d'authentification
+```sh
+symfony console make:auth
+```
--- migrations/Version20230705095919.php
+++ migrations/Version20230705095919.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230703133440 extends AbstractMigration
+final class Version20230705095919 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230703133440 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post ADD slug VARCHAR(255) NOT NULL, ADD visible TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE `admin` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_880E0D76F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE post DROP slug, DROP visible');
+        $this->addSql('DROP TABLE `admin`');
     }
 }
