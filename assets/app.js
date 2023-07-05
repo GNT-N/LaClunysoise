bd0196fe1d1eaceb217db1c6db2076300a07d613
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-05 15:42:06 +0200

    Update

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,16 @@
 import '../assets/styles/app.scss';
+
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+document.addEventListener('turbo:load', function (e) {
+
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    });
+});
