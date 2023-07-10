984222b3b0d39642ace440fdef68831d39693654
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-10 14:54:03 +0200

    comment's

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,32 +14,49 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     public function __construct(
-        private AdminUrlGenerator $adminUrlGenerator
-        ) {
-    }
-
-    #[Route('/admin', name: 'admin')]
+        // Injection de dépendance pour AdminUrlGenerator
+        private AdminUrlGenerator $adminUrlGenerator 
+    ) {}
+    
+    // Annotation de route pour la méthode index()
+    #[Route('/admin', name: 'admin')] 
     public function index(): Response
     {
         // return parent::index();
-        $url = $this->adminUrlGenerator
-        ->setController(PostCrudController::class)
-        ->generateUrl();
 
-        return $this->redirect($url);
+        // Stockage de l'URL générée par AdminUrlGenerator
+        $url = $this->adminUrlGenerator 
+
+            // Définition du contrôleur pour l'URL générée
+            ->setController(PostCrudController::class) 
+            // Génération de l'URL
+            ->generateUrl(); 
+    
+        // Redirection vers l'URL générée
+        return $this->redirect($url); 
     }
-
+    
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()
+        // Création d'une nouvelle instance de Dashboard
+        return Dashboard::new() 
+            // Définition du titre du tableau de bord
             ->setTitle('La Clunysoise Administration'); 
     }
-        public function configureMenuItems(): iterable
-        {
-            yield MenuItem::subMenu('Publications', 'fa fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('Nouveau post', 'fas fa-plus', Post::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Voir post', 'fas fa-eye', Post::class),
-            MenuItem::linkToUrl('Accéder au site', 'fa fa-globe', '/')
-        ]);
-        }
+    
+    public function configureMenuItems(): iterable
+    {
+        // Création d'un sous-menu avec le libellé "Publications" et la classe CSS "fa fa-newspaper"
+        yield MenuItem::subMenu('Publications', 'fa fa-newspaper') 
+            // Configuration des sous-éléments du sous-menu    
+            ->setSubItems([ 
+                // Lien CRUD "Nouveau post" avec une icône et une action spécifiées
+                MenuItem::linkToCrud('Nouveau post', 'fas fa-plus', Post::class)->setAction(Crud::PAGE_NEW),
+                // Lien CRUD "Voir post" avec une icône spécifiée 
+                MenuItem::linkToCrud('Voir post', 'fas fa-eye', Post::class),
+                // Lien vers une URL externe avec une icône spécifiée 
+                MenuItem::linkToUrl('Accéder au site', 'fa fa-globe', '/') 
+            ]);
+    }
+    
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,30 +8,40 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PostRepository;
 
-#[Route('/', name: 'home')]
+// Annotation de route pour la classe HomeController
+#[Route('/', name: 'home')] 
 class HomeController extends AbstractController
 {
-    #[Route('/{slug}', name: 'show', methods: ['GET'])]
+    // Annotation de route pour la méthode show()
+    #[Route('/{slug}', name: 'show', methods: ['GET'])] 
     public function show(Post $post): Response
     {
-        return $this->render('post/show.html.twig', [
-            'post' => $post,
+        // Rendu du template 'post/show.html.twig'
+        return $this->render('post/show.html.twig', [ 
+            // Passage de la variable 'post' au template
+            'post' => $post, 
         ]);
     }
 
-    #[Route('/', name: 'main')]
+    // Annotation de route pour la méthode acceuil()
+    #[Route('/', name: 'main')] 
     public function acceuil(PostRepository $postRepository): Response
     {
-        return $this->render('home/accueil.html.twig', [
-            'post' => $postRepository->findBy(array('page' => 'accueil', 'visible' => true)),
+        // Rendu du template 'home/accueil.html.twig'
+        return $this->render('home/accueil.html.twig', [ 
+            // Récupération des entités 'Post' par le biais du 'PostRepository' avec des critères de recherche spécifiques
+            'post' => $postRepository->findBy(array('page' => 'accueil', 'visible' => true)), 
         ]);
     }
 
-    #[Route('/about', name: 'about')]
+    // Annotation de route pour la méthode about()
+    #[Route('/about', name: 'about')] 
     public function about(PostRepository $postRepository): Response
     {
-        return $this->render('/about.html.twig', [
-            'post' => $postRepository->findBy(array('page' => 'a-propos', 'visible' => true)),
+        // Rendu du template '/about.html.twig'
+        return $this->render('/about.html.twig', [ 
+            // Récupération des entités 'Post' par le biais du 'PostRepository' avec des critères de recherche spécifiques
+            'post' => $postRepository->findBy(array('page' => 'a-propos', 'visible' => true)), 
         ]);
     }
 }
