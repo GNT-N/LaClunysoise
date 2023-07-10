d99d989288c3a3ddb4e623d0837dd5fe682e87b9
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-10 18:07:58 +0200

    template's organisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,8 @@
 // Déclaration du namespace du contrôleur
 namespace App\Controller;
 
-
+// Importation de la classe d'entité Post
+use App\Entity\Post;
 // Importation de la classe de base pour les contrôleurs Symfony
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 // Importation de la classe Response pour la réponse HTTP
@@ -22,17 +23,17 @@ class HomeController extends AbstractController
     public function acceuil(PostRepository $postRepository): Response
     {
         // Rendu du template 'home/accueil.html.twig' avec les posts filtrés par page et visibilité
-        return $this->render('home/accueil.html.twig', [
+        return $this->render('main/accueil.html.twig', [
             'post' => $postRepository->findBy(array('page' => 'accueil', 'visible' => true)),
         ]);
     }
 
     // Définition de la route '/about' avec le nom 'about'
-    #[Route('/about', name: 'about')]
+    #[Route('/a-propos', name: 'about')]
     public function about(PostRepository $postRepository): Response
     {
         // Rendu du template '/about.html.twig' avec les posts filtrés par page et visibilité
-        return $this->render('/about.html.twig', [
+        return $this->render('main/about.html.twig', [
             'post' => $postRepository->findBy(array('page' => 'a-propos', 'visible' => true)),
         ]);
     }
