2c97afaea3102127f9e295e07b42f09954c1ac70
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-18 15:01:19 +0200

    update form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -13,9 +14,25 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('content', TextareaType::class)
-            ->add('envoyer', SubmitType::class)
+            ->add('Nom',TextType::class, [
+                'attr' => ['class' => 'form-control mt-4', 'placeholder' => 'Nom'],
+                'label' => ' ',
+            ])
+            ->add('Prenom', TextType::class, [
+                'attr' => ['class' => 'form-control mt-4', 'placeholder' => 'Prenom'],
+                'label' => ' ',
+            ])
+            ->add('email', TextType::class, [
+                'attr' => ['class' => 'form-control mt-4', 'placeholder' => 'email'],
+                'label' => ' ',
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['class' => 'form-control mt-4', 'placeholder' => 'Contenu'],
+                'label' => ' ',
+            ])
+            ->add('envoyer', SubmitType::class, [
+                'attr' => ['class' => 'form-control mt-4'],
+            ])
         ;
     }
 
