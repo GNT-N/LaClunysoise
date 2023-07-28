cc793725bbd64b4201017f41f34d694aad743b39
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-28 16:30:24 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,15 @@ const $ = require('jquery');
 
 // Chargement de la partie JS de bootstrap
 require('bootstrap');
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
