ba5551a30fc2251374892fa321669da4964b08eb
Nicolas Gonot <gg_nicolas@hotmail.com>
2023-07-30 14:10:45 +0200

    update dashboard

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -26,6 +26,13 @@ use Symfony\Component\Routing\Annotation\Route;
 // Importation de la classe SluggerInterface pour la génération de slugs
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 #[Route('/',  name: 'app_posts_' )]
 
@@ -37,139 +44,169 @@ class PostCrudController extends AbstractCrudController
     {
         return Post::class;
     }
-        // Constantes pour le chemin de base et le répertoire de téléversement des images
-        public const POSTS_BASE_PATH = 'upload/images';
-        public const POSTS_UPLOAD_DIR = 'public/upload/images';
-     
-        // Configuration des champs de formulaire pour les différentes pages
-        public function configureFields(string $pageName): iterable
-        {
-            return [
-                // Champ d'identifiant (masqué sur le formulaire et l'index)
-                IdField::new('id')
-                ->hideOnForm()
-                ->hideOnIndex(),
 
-                // Champ boolean pour la visibilité (affiché comme un interrupteur)
-                BooleanField::new('visible', 'Visible')
-                ->renderAsSwitch(true),
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
 
-                // Champ d'image pour l'URL du média (avec chemin de base et répertoire de téléversement)
-                ImageField::new('mediaUrl')
-                ->setBasePath(self::POSTS_BASE_PATH)
-                ->setUploadDir(self::POSTS_UPLOAD_DIR)
-                ->setUploadedFileNamePattern('[slug]-[uuid].[extension]'),
+        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+            return $action->setLabel('Modifier');
+        })
 
-                // Champ de choix pour la page (affiché sous forme de liste déroulante)
-                ChoiceField::new('page', 'Page')
-                ->setChoices([
-                    'Accueil' => 'accueil',
-                    'Notre identité' => 'notre-identite',
-                    'Prise en charge' => 'prise-en-charge',
-                    'Nous rejoindre' => 'nous-rejoindre',
-                ])
-                ->renderExpanded(),
+        ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+            return $action->setLabel('Supprimer');
+        })
 
-                // Champ de texte pour le titre
-                TextField::new('title', 'Titre'),
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setLabel('Sauvegarder et quitter');
+        })
 
-                // Champ de texte pour la description
-                TextField::new('description', 'Description'),
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action->setLabel('Sauvegarder et continuer');
+        })
+        
+        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setLabel('Sauvegarder et quitter');
+        })
 
-                // Champ d'éditeur de texte pour le contenu
-                TextEditorField::new('content', 'Contenu')
-                    ->setFormTypeOption('attr', ['class' => 'tinymce']),
+        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $action) {
+            return $action->setLabel('Sauvegarder et continuer');
+        });
+    }
 
-                // Champ de date/heure pour la création (masqué sur le formulaire)
-                DateTimeField::new('createdAt', 'Date/Heure de Création')
-                ->hideOnForm()
-                ->setTimezone('Europe/Paris'),
-                
-                // Champ de date/heure pour la modification (masqué sur le formulaire)
-                DateTimeField::new('updatedAt', 'Date/Heure de Modification')
-                ->hideOnForm()
-                ->setTimezone('Europe/Paris'),
-
-                // Champ de slug généré à partir du titre (masqué sur le formulaire et l'index)
-                SlugField::new('slug')
-                ->setTargetFieldName('title')
-                ->hideOnForm()
-                ->hideOnIndex(),
-                
-            ];
-        }
-
-        // Méthode pour persister une entité dans la base de données
-        public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
-        {
-            // Vérification s'il s'agit d'une instance de l'entité Post (ne pas persister)
-            if (!$entityInstance instanceof Post) return;
+    // Constantes pour le chemin de base et le répertoire de téléversement des images
+    public const POSTS_BASE_PATH = 'upload/images';
+    public const POSTS_UPLOAD_DIR = 'public/upload/images';
     
-            // Définition de la date de création
-            $entityInstance->setCreatedAt(new \DateTimeImmutable);
+    // Configuration des champs de formulaire pour les différentes pages
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            // Champ d'identifiant (masqué sur le formulaire et l'index)
+            IdField::new('id')
+            ->hideOnForm()
+            ->hideOnIndex(),
 
-            // Génération du slug à partir du titre du post
-            $slug = $this->slugger->slug($entityInstance->getTitle())->lower();
-            $entityInstance->setSlug($slug);        
-    
-            // Appel de la méthode parent pour persister l'entité
-            parent::persistEntity($entityManager, $entityInstance);
-        }
+            // Champ boolean pour la visibilité (affiché comme un interrupteur)
+            BooleanField::new('visible', 'Visible')
+            ->renderAsSwitch(true),
 
+            // Champ d'image pour l'URL du média (avec chemin de base et répertoire de téléversement)
+            ImageField::new('mediaUrl')
+            ->setBasePath(self::POSTS_BASE_PATH)
+            ->setUploadDir(self::POSTS_UPLOAD_DIR)
+            ->setUploadedFileNamePattern('[slug]-[uuid].[extension]'),
 
-        // Méthode pour mettre à jour une entité dans la base de données
-        public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-        {
-            // Vérification s'il s'agit d'une instance de l'entité Post (ne pas mettre à jour)
-            if (!$entityInstance instanceof Post) return;
-    
-            // Mise à jour de la date de modification
-            $entityInstance->setUpdatedAt(new \DateTimeImmutable);      
-    
-            // Appel de la méthode parent pour mettre à jour l'entité
-            parent::updateEntity($entityManager, $entityInstance);
-        }
+            // Champ de choix pour la page (affiché sous forme de liste déroulante)
+            ChoiceField::new('page', 'Page')
+            ->setChoices([
+                'Accueil' => 'accueil',
+                'Notre identité' => 'notre-identite',
+                'Prise en charge' => 'prise-en-charge',
+                'Nous rejoindre' => 'nous-rejoindre',
+            ])
+            ->renderExpanded(),
 
+            // Champ de texte pour le titre
+            TextField::new('title', 'Titre'),
 
-        // Méthode pour supprimer une entité de la base de données
-        public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
-        {
-            // Vérification s'il s'agit d'une instance de l'entité Post
-            if (!$entityInstance instanceof Post) {
-                return;
-            }
+            // Champ de texte pour la description
+            TextField::new('description', 'Description'),
 
-            // Récupération du nom de l'image
-            $imageName = $entityInstance->getMediaUrl();
+            // Champ d'éditeur de texte pour le contenu
+            TextEditorField::new('content', 'Contenu')
+                ->setFormTypeOption('attr', ['class' => 'tinymce']),
+
+            // Champ de date/heure pour la création (masqué sur le formulaire)
+            DateTimeField::new('createdAt', 'Date/Heure de Création')
+            ->hideOnForm()
+            ->setTimezone('Europe/Paris'),
             
-            // Vérification et suppression de l'image associée
-            if ($imageName) {
-                $imagePath = $this->getParameter('kernel.project_dir'). '/public/upload/images/'. $imageName;
-                if (file_exists($imagePath)) {
-                    unlink($imagePath);
-                }
+            // Champ de date/heure pour la modification (masqué sur le formulaire)
+            DateTimeField::new('updatedAt', 'Date/Heure de Modification')
+            ->hideOnForm()
+            ->setTimezone('Europe/Paris'),
+
+            // Champ de slug généré à partir du titre (masqué sur le formulaire et l'index)
+            SlugField::new('slug')
+            ->setTargetFieldName('title')
+            ->hideOnForm()
+            ->hideOnIndex(),
+            
+        ];
+    }
+
+    // Méthode pour persister une entité dans la base de données
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Vérification s'il s'agit d'une instance de l'entité Post (ne pas persister)
+        if (!$entityInstance instanceof Post) return;
+
+        // Définition de la date de création
+        $entityInstance->setCreatedAt(new \DateTimeImmutable);
+
+        // Génération du slug à partir du titre du post
+        $slug = $this->slugger->slug($entityInstance->getTitle())->lower();
+        $entityInstance->setSlug($slug);        
+
+        // Appel de la méthode parent pour persister l'entité
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+
+    // Méthode pour mettre à jour une entité dans la base de données
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Vérification s'il s'agit d'une instance de l'entité Post (ne pas mettre à jour)
+        if (!$entityInstance instanceof Post) return;
+
+        // Mise à jour de la date de modification
+        $entityInstance->setUpdatedAt(new \DateTimeImmutable);      
+
+        // Appel de la méthode parent pour mettre à jour l'entité
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+
+    // Méthode pour supprimer une entité de la base de données
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Vérification s'il s'agit d'une instance de l'entité Post
+        if (!$entityInstance instanceof Post) {
+            return;
+        }
+
+        // Récupération du nom de l'image
+        $imageName = $entityInstance->getMediaUrl();
+        
+        // Vérification et suppression de l'image associée
+        if ($imageName) {
+            $imagePath = $this->getParameter('kernel.project_dir'). '/public/upload/images/'. $imageName;
+            if (file_exists($imagePath)) {
+                unlink($imagePath);
             }
+        }
 
-            // Appel de la méthode parent pour supprimer l'entité
-            parent::deleteEntity($entityManager, $entityInstance);
-        }
-        
-        // Définition de la route '/articles/{slug}' pour afficher un post spécifique
-        #[Route('/{slug}', name: 'show', methods: ['GET'])]
-        public function show(Post $post): Response
-        {
-            return $this->render('post/show.html.twig', [
-                'post' => $post,
-            ]);
-        }
-        
-        // Propriété privée pour l'objet SluggerInterface utilisé pour générer les slugs
-        private $slugger;
+        // Appel de la méthode parent pour supprimer l'entité
+        parent::deleteEntity($entityManager, $entityInstance);
+    }
+    
+    // Définition de la route '/articles/{slug}' pour afficher un post spécifique
+    #[Route('/{slug}', name: 'show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+    
+    // Propriété privée pour l'objet SluggerInterface utilisé pour générer les slugs
+    private $slugger;
 
-        // Constructeur du contrôleur, injecte l'objet SluggerInterface
-        public function __construct(SluggerInterface $slugger)
-        {
-            $this->slugger = $slugger;
-        }
+    // Constructeur du contrôleur, injecte l'objet SluggerInterface
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
 
 }
